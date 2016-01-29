301df802e28589c20aecce731d60479b3d31a7f2
Dan Ferris <dan@usrsbin.com>
2016-01-29 11:54:48 -0700

    Added WSGI glue Added start sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #activate the virtualenv
-source /home/dferris/otd/bin/activate
+source /home/dferris/otd_git/bin/activate
 
 PORT=4443
 IP=127.0.0.1
@@ -9,6 +9,7 @@ PROCESSES=4
 BUFFER_SIZE=65536
 LOGFILE="otd.log"
 PIDFILE="otd.pid"
-VIRTUALENV=""
+VIRTUALENV="/home/dferris/otd_git"
 
+source $VIRTUALENV/activate
 uwsgi --socket $IP:$PORT -w WSGI:app --processes $PROCESSES --buffer-size $BUFFER_SIZE --master --daemonize2 $LOGFILE --pidfile $PIDFILE --virtualenv $VIRTUALENV
